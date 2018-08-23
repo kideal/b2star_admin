50f28e0f3f91638f39c91f7f1c9f3b06528abeb7
huangtao <571984979@qq.com>
2018-08-23 15:06:29 +0800

    添加poi事件模式

--- src/main/java/com/mall/utils/selenium/selenium/LuanchChrome.java
+++ src/main/java/com/mall/utils/selenium/selenium/LuanchChrome.java
@@ -2,6 +2,8 @@ package com.mall.utils.selenium.selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,9 +12,8 @@ import java.util.concurrent.TimeUnit;
  */
 public class LuanchChrome {
     public static void main(String[] args) {
-        String path = "C:\\project\\b2star_admin\\src\\main\\java\\com\\mall\\utils\\selenium\\selenium\\geckodriver.exe";
-        System.out.println(path);
-        System.setProperty("webdriver.chrome.driver", path);
+
+        System.setProperty("webdriver.chrome.driver", "C:\\project\\b2star_admin\\src\\main\\java\\com\\mall\\utils\\selenium\\selenium\\chromedriver.exe");
 
         //初始化一个chrome浏览器实例，实例名称叫driver
         WebDriver driver = new ChromeDriver();
@@ -22,12 +23,13 @@ public class LuanchChrome {
         driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
 
         // get()打开一个站点
-        driver.get("http://www.b2star.com//");
+        driver.get("https://www.baidu.com");
         //getTitle()获取当前页面title的值
-        System.out.println("当前打开页面的标题是： "+ driver.toString());
+        System.out.println("当前打开页面的标题是： "+ driver.getTitle());
 
         //关闭并退出浏览器
         driver.quit();
 
     }
+
 }
