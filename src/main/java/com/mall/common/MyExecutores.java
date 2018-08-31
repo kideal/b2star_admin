05eea6a47e273dba92ca4f23e82548c8a5fdd2ed
huangtao <571984979@qq.com>
2018-08-31 11:59:13 +0800

    完全体

--- src/main/java/com/mall/common/MyExecutores.java
+++ src/main/java/com/mall/common/MyExecutores.java
@@ -1,6 +1,5 @@
 package com.mall.common;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
